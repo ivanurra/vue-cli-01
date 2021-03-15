936b75b2ebcf25d25ef228589d3e875eea33b6d0
Ivan Urra <ivan.urra.wd@gmail.com>
2021-03-15 08:20:09 +0100

    add app.vue styles.css

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+            <ul>
+                <li></li>
+            </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'maria',
+                    name: 'Maria Colina',
+                    phone: '655 555 555',
+                    email: 'maria.colina@localhost.com'
+                },
+                {
+                    id: 'pep',
+                    name: 'Pep Capdepera',
+                    phone: '655 555 555',
+                    email: 'pep.capdepera@localhost.com'
+                }
+            ]
+        };
+    },
+    // methods: {},
+    // computed: {}
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import './styles.css';
 
-createApp({}).mount('');
+createApp(App).mount('#app');
