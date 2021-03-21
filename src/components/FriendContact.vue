b6a3bc4aa9faa7da14aed5efe423590d49e63579
Ivan Urra <ivan@auro-group.com>
2021-03-21 08:54:42 +0100

    emits

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -39,22 +39,22 @@ export default {
             }
         }
     },
-    emits: ['toggle-favorite'],
-    // emits: {
-    //     'toggle-favorite': function(id) {
-    //         if (id) {
-    //             return true;
-    //         } else {
-    //             console.warn('id is missing!');
-    //             return false;
-    //         }
-    //     }
-    // },
-    // data() {
-    //     return {
-    //         detailsAreVisible: false
-    //     };
-    // },
+    // emits: ['toggle-favorite'],
+    emits: {
+        'toggle-favorite': function(id) {
+            if (id) {
+                return true;
+            } else {
+                console.warn('id is missing!');
+                return false;
+            }
+        }
+    },
+    data() {
+        return {
+            detailsAreVisible: false
+        };
+    },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
